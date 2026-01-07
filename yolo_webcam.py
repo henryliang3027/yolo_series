0eb2de1d728ca2783a651b65d4774e7ef90a3854
henryliang <henryliang3027@gmail.com>
2026-01-07 14:13:01 +0800

    update bounding box drawing logic

--- yolo_webcam.py
+++ yolo_webcam.py
@@ -1,8 +1,9 @@
 import cv2
 from ultralytics import YOLO
+import math
 
 # specify the model path
-model_path = "finetuned_weights/yolov11s.pt"
+model_path = "finetuned_weights/best_4_class_20260107.pt"
 # Initialize a YOLOE model
 model = YOLO(model_path)  # or select yoloe-11s/m-seg.pt for different sizes
 
@@ -11,6 +12,9 @@ model = YOLO(model_path)  # or select yoloe-11s/m-seg.pt for different sizes
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
+
+names = {0:"c0",1:"c1",2:"c2",3:"c3"}
+colors = {0:(0, 255, 255),1:(41, 207, 0),2:(0, 161, 255),3:(255, 242, 0)}
 
 while True:
     # Read a frame from the webcam
@@ -23,15 +27,40 @@ while True:
 
     # Run YOLO detection on the current frame
     # The 'stream=True' argument is important for real-time processing
-    results = model(frame, stream=True)
+    results = model(frame, conf=0.6, stream=True)
 
     # Process and display the results
-    for r in results:
-        # 'plot()' method draws bounding boxes, labels, and confidence scores directly on the frame
-        annotated_frame = r.plot()
-        cv2.imshow("YOLO Webcam Detection", annotated_frame)
+
+    r = list(results)[0]
+    boxes = r.boxes.xyxy
+    class_ids = r.boxes.cls
+    confs = r.boxes.conf
+
+    for box, class_id, conf in zip(boxes, class_ids, confs):
+        # put box in cam
+        x1,y1,x2,y2 = box.tolist()
+        x1,y1,x2,y2 = int(x1),int(y1),int(x2),int(y2)
+
+        cv2.rectangle(frame, (x1, y1), (x2, y2), colors[label_id], 3)
+
+
+        # object details
+        org = [x1, y1 - 6]
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        fontScale = 0.6
+
+        thickness = 2
+
+
+        cv2.putText(frame, f'{names[label_id]} {conf:.2f}', org, font, fontScale, colors[label_id], thickness)
+
+    # for r in results:
+    #     # 'plot()' method draws bounding boxes, labels, and confidence scores directly on the frame
+    #     annotated_frame = r.plot()
+    #     cv2.imshow("YOLO Webcam Detection", annotated_frame)
 
     # Break the loop if 'q' is pressed
+    cv2.imshow("YOLO Webcam Detection", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
